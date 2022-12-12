0668b2e7a78a35522eba4a3d6977746c49341f16
Vertical <ambintasso@gmail.com>
2022-12-12 19:46:45 +0530

    html

---
+++ script.js
@@ -0,0 +1,8 @@
+let container = document.getElementsById("container");
+let grid = document.getElementsById("grid");
+let size = 16;
+
+function makegrid() {
+  let cell = document.createElement("div").id("cell");
+  container.appendChild(cell);
+}
