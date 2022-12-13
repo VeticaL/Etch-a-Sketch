58da3553b2e2ba59a9327dad123fcb85bd1ce063
Vertical <ambintasso@gmail.com>
2022-12-13 19:32:09 +0530

    added grid-container

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-let container = document.getElementsById("container");
-let grid = document.getElementsById("grid");
-let size = 16;
+const container = document.getElementById("grid-container");
+const colorPicker = document.getElementById("colorPicker");
+const colorBtn = document.getElementById("colorBtn");
+const rainbowBtn = document.getElementById("rainbowBtn");
+const eraserBtn = document.getElementById("eraserBtn");
+const clearBtn = document.getElementById("clearBtn");
+const sizeValue = document.getElementById("sizeValue");
+const sizeSlider = document.getElementById("sizeSlider");
 
-function makegrid() {
-  let cell = document.createElement("div").id("cell");
-  container.appendChild(cell);
+function makegrid(size) {
+  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  for (let i = 0; i < size * size; i++) {
+    let grid = document.createElement("div");
+    grid.classList.add("grid-container");
+    container.appendChild(grid);
+    grid.style.backgroundColor = "blue";
+  }
 }
+
+makegrid(16)
+
+function changeSize() {
+ let newSize = sizeSlider.value;
+ sizeValue.innerHTML = `${newSize} X ${newSize}`;
+ console.log(newSize)
+}
+
+changeSize()
