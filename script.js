516b86d87f45d23b6529e439fbca6aaebb6df830
Vertical <ambintasso@gmail.com>
2022-12-14 20:48:47 +0530

    Randomcolor mode added

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const container = document.getElementById("grid-container");
 const colorPicker = document.getElementById("colorPicker");
 const colorBtn = document.getElementById("colorBtn");
 const rainbowBtn = document.getElementById("rainbowBtn");
+const randomBtn = document.getElementById("randomBtn");
 const eraserBtn = document.getElementById("eraserBtn");
 const clearBtn = document.getElementById("clearBtn");
 const sizeValue = document.getElementById("sizeValue");
@@ -9,6 +10,7 @@ const sizeSlider = document.getElementById("sizeSlider");
 
 let color = "#333333";
 let girdsize = 16;
+let mode = "colorMode";
 
 function makegrid(size) {
   container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
@@ -18,17 +20,22 @@ function makegrid(size) {
     grid.classList.add("grid-container");
     container.appendChild(grid);
     grid.style.backgroundColor = "White";
-    grid.addEventListener("mousedown", changeColor)
-    grid.addEventListener("mouseover", changeColor)
+    grid.addEventListener("mousedown", changeColor);
+    grid.addEventListener("mouseover", changeColor);
   }
 }
-
-makegrid(16);
 
 // change grid color
 
 function changeColor() {
-  this.style.backgroundColor = color
+  this.style.backgroundColor = color;
+  // Rainbow Mode
+  if (mode === "rainbowMode") {
+    let R = Math.floor(Math.random() * 256);
+    let G = Math.floor(Math.random() * 256);
+    let B = Math.floor(Math.random() * 256);
+    this.style.backgroundColor = `rgb(${R}, ${B}, ${G})`;
+  }
 }
 
 // change gridSize
@@ -37,37 +44,43 @@ sizeSlider.oninput = function () {
   girdsize = this.value;
   sizeValue.innerHTML = `${girdsize} X ${girdsize}`;
   makegrid(girdsize);
-}
+};
 
 // color Picker
 
 colorPicker.oninput = function () {
   let defaultColor = colorBtn.value;
   let newColor = this.value;
-  console.log(newColor);
-  if (newColor !== defaultColor) {
-    color = newColor
-  }
-}
+  color = newColor;
+};
 
 //Buttons
 
 colorBtn.onclick = function () {
+  mode = "colorMode";
   color = "#333333";
-}
+};
 
 eraserBtn.onclick = function () {
+  mode = "eraserMode";
   color = "White";
-}
+};
 
-clearBtn.onclick = function() {
+clearBtn.onclick = function () {
+  mode = "clearMode";
   container.innerHTML = "";
   makegrid(girdsize);
-}
+};
 
 rainbowBtn.onclick = function () {
-  let R = Math.floor(Math.random() * 256);
-  let G = Math.floor(Math.random() * 256);
-  let B = Math.floor(Math.random() * 256);
-  color = `rgb(${R}, ${B}, ${G})`;
+  mode = "rainbowMode";
+};
+
+randomBtn.onclick = function() {
+  mode = "fillMode";
+  color = `hsl(${Math.random() * 360}, 100%, 50%`;
 }
+
+window.onload = () => {
+  makegrid(girdsize);
+};
